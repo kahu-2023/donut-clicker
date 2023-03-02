55f8ecd06f42cd99d7372b5fb88bc591267525ab
Emily Waters <erwaters92@gmail.com>
2023-03-03 10:33:12 +1300

    Variables declared, event listener added, increment function created

--- server/client.js
+++ server/client.js
@@ -1,6 +1,34 @@
 //Emily
 
+// Total score variable
 
+let totalScore = 0
+
+//Increment variable
+
+let scoreIncrement = 1
+
+//Get element by ID to identify the donut
+
+let donut = document.getElementById('donut')
+
+//Attach an event listener to the donut that runs the increment to increase the score.
+
+donut.addEventListener('click', increment)
+
+//Function to increase score.
+
+function increment() {
+    totalScore += scoreIncrement 
+}
+
+//Function to add increment to score total.
+
+function totalScore() {
+    
+}
+
+//Update total score in text content.
 
 
 
