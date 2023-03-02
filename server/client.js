ccebe8b801fbaaf2e0bf65535d1e619a22bc718f
Emily Waters <erwaters92@gmail.com>
2023-03-03 09:57:04 +1300

    Delete server side code from client.js

--- server/client.js
+++ server/client.js
@@ -1,20 +1,9 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const { isUtf8 } = require('node:buffer')
-const server = express()
-const fs = require('node:fs/promises')
-// const routes = require('./routes.js')
+//Emily
 
-// Server configuration
-server.use(express.static(__dirname + '/public'))
-// gives server access to the public folder w/o need for abs file path
-server.use(express.urlencoded({ extended: false }))
 
-// Handlebars configuration
-server.engine('hbs', hbs.engine({ extname: 'hbs' }))
-server.set('view engine', 'hbs')
-server.set('views', __dirname + '/views')
 
-// Routes
 
-module.exports = client
+
+
+
+//Ryan
