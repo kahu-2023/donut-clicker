f5c18f3178dd3ebb9fbfff9a30f4bab48d57a33e
Shirley Park <shirley.park7@gmail.com>
2023-03-03 09:51:23 +1300

    add server

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const { isUtf8 } = require('node:buffer')
+const server = express()
+const fs = require('node:fs/promises')
+// const routes = require('./routes.js')
+
+// Server configuration
+server.use(express.static(__dirname + '/public'))
+// gives server access to the public folder w/o need for abs file path
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+server.set('views', __dirname + '/views')
+
+// server.use('/puppies', routes)
+
+// Routes
+
+server.get('/game', (req, res) => {
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+    .then((player) => {
+      res.render('gamepage')
+    })
+    .catch((err) => {
+      res.status(404).send(err.message)
+    })
+})
+
+server.get('/', (req, res) => {
+  // res.send('Pupparazzi')
+
+  // read puppies from data.json
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+    .then((data) => {
+      const parsedData = JSON.parse(data)
+      // console.log(parsedData)
+      // returns data object array
+
+      res.render('home', parsedData)
+    })
+    .catch((err) => res.status(400).send(err.message))
+})
+
+module.exports = server
